239312c0dba97b69aa03d51769ec4f3d8e624717
Abhishek Kumar <sahniak56@gmail.com>
2024-01-12 15:12:32 +0530

    db

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connect } from 'mongoose';
 import connectDB from './db/db.js';
 
-
-
-
-
+// load env variables
 
 dotenv.config();
+
 const app = express();
+
 
 // database connection
 connectDB();
 
 
-
 app.get('/', (req, res) => {
+    const date = new Date();
+    const options = { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' };
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+
+    const user=[
+        {
+            name: 'John Doe',
+            email: 'john@gmail.com'
+        },
+        {
+            name: 'Abhishek',
+            email: 'abhi@gmail.com'
+        }
+    ]
+
+
     res.status(200).json({
         success: true,
-        message: 'Welcome to ExpressJS starter kit!',
+        message: 'Welcome to Precogs',
+        version: '1.0.0',
+        time: formattedDate,
+        user: user
+
     })
 });
+
 
 const port = process.env.PORT || 8000;
 
